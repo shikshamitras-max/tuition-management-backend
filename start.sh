e6bace0ab9f25674815154270a710056d0f4ca27
Pranjal <pranjalmoradiya24@gmail.com>
2026-01-10 17:47:52 +0530

    Auto initialize database on server start

--- start.sh
+++ start.sh
@@ -1,1 +1,10 @@
+#!/usr/bin/env bash
+echo "Initializing database..."
+
+python init_db.py
+python init_exam_db.py
+python init_institute.py
+python setup_institute.py
+
+echo "Starting server..."
 gunicorn app:app
